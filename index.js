9bcffb0cd6c3fb2058d1cf7a82b0afa7bf422853
Arihant Jain <66168181+arihant-jain-09@users.noreply.github.com>
2022-04-11 17:33:32 +0530

    added /get route

--- index.js
+++ index.js
@@ -32,18 +32,22 @@ require('./routes/User/authRoutes')(app);
 require('./routes/Company/authRoutes')(app);
 require('./routes/Invoice/routes')(app);
 
-if(process.env.NODE_ENV ==='production'){
-  app.use(express.static('client/build'));
-  const path=require('path');
-  app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-  })
-}
+// if(process.env.NODE_ENV ==='production'){
+//   app.use(express.static('client/build'));
+//   const path=require('path');
+//   app.get('*',(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+//   })
+// }
 const PORT=process.env.PORT || 5000;
 app.listen(PORT);
 
 app.use('/api/uptime', require('express-healthcheck')({
   healthy: serverStatus
 }));
+
+app.get('/',(req,res)=>{
+  res.send('Welcome to B2B transact')
+})
 
 
